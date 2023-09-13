cebee789d8ffd8efdda496cdfb9f5f881a7c61ba
BrunoBustos96 <samael8496@gmail.com>
2023-09-13 17:45:53 -0600

    Update: Describe your changes here

--- modules/ttl_signals.py
+++ modules/ttl_signals.py
@@ -1,3 +1,4 @@
+"""
 import parallel
 
 # Find the first parallel port (usually "/dev/parport0")
@@ -15,3 +16,52 @@ else:
 
     # Close the parallel port
     port.close()
+"""
+
+# ttl_signals.py
+
+from time import sleep
+import random
+from expyriment.io import ParallelPort
+
+# Initialize the Parallel Port; replace '/dev/parport0' with the actual device path on your machine
+pp = ParallelPort('/dev/parport0')
+
+def generate_pseudo_random_time(lower=5, upper=7):
+    return random.uniform(lower, upper)
+
+def send_ttl_signal(signal_code):
+    """
+    Sends a TTL signal with the given code using expyriment's ParallelPort.
+    """
+    pp.set_data(signal_code)
+    sleep(0.002)  # 2ms pulse
+    pp.set_data(0)  # Reset to 0
+
+def initiate_trial():
+    intertrial_time = generate_pseudo_random_time()
+    holding_time = 1.0  # in seconds
+
+    # Start of Trial
+    send_ttl_signal(255)  # You can adjust this code as needed
+
+    # Holding Time
+    sleep(holding_time)
+
+    # Inter-trial Time
+    sleep(intertrial_time)
+
+    # End of Trial
+    send_ttl_signal(0)  # You can adjust this code as needed
+
+    return intertrial_time
+
+if __name__ == "__main__":
+    # Test code for the ttl_signals.py module
+    random.seed(None)  # Seed with current system time for more randomness
+
+    print("Starting test trials...")
+    for i in range(3):
+        intertrial_time = initiate_trial()
+        print(f"Trial {i+1} completed with intertrial time: {intertrial_time} seconds")
+    print("All test trials completed.")
